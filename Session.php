2c0e08cc95ebd0a013daa7b24088f2888fa5cfea
n302697 <christian.abila@n.roteskreuz.at>
2020-11-30 12:44:19 +0100

    Added DocBlock comments

--- Session.php
+++ Session.php
@@ -1,18 +1,23 @@
 <?php
 
 /**
- * Singleton?
+ * This class provides methods to handle PHP Sessions
+ * Sessions can be started and closed
  */
 class Session
-{ 
+{
+    /**
+     * Get a PHP Session
+     */
     public static function get()
     {
         if(self::status() === PHP_SESSION_NONE)
-            self::start();
-        
-        
+            self::start();  
     }
 
+    /**
+     * Closes an active PHP Session
+     */
     public static function close()
     {
         if(self::status() === PHP_SESSION_ACTIVE)
